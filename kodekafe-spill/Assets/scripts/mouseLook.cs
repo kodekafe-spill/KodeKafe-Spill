adb8a81156e21437a60aae4c754b748c17bd0dd3
Nikolai Dale Milde <ndalemilde@outlook.com>
2020-02-08 23:40:19 +0100

    New things

---
+++ kodekafe-spill/Assets/scripts/mouseLook.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+    }
+}
