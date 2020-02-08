dbc6bd81be7591f5def5097b4efca38102fd179f
Nikolai Dale Milde <ndalemilde@outlook.com>
2020-02-08 23:47:08 +0100

    You can look around
    You can look but not move

--- kodekafe-spill/Assets/scripts/mouseLook.cs
+++ kodekafe-spill/Assets/scripts/mouseLook.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class mouseLook : MonoBehaviour
 {
+
+    public float mouseSens = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
     void Start()
     {
         
@@ -11,7 +18,12 @@ public class mouseLook : MonoBehaviour
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
