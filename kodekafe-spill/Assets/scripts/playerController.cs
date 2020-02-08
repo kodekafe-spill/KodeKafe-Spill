6e73c20fd484ba7cbba90717bd3c1f150db2cab7
Nikolai Dale Milde <ndalemilde@outlook.com>
2020-02-08 23:27:30 +0100

    Update playerController.cs

--- kodekafe-spill/Assets/scripts/playerController.cs
+++ kodekafe-spill/Assets/scripts/playerController.cs
@@ -6,9 +6,6 @@ public class playerController : MonoBehaviour
 {
     public Rigidbody rb;
 
-    public float speed = 6f;
-    public float jump = 3f;
-
     void Start()
     {
         rb = GetComponent<Rigidbody>();
