be30eedf54d783072942c9e47da5134e3db5e211
Nikolai Dale Milde <ndalemilde@outlook.com>
2020-02-09 10:19:17 +0100

    Added fov slider and pause menu (esc)

---
+++ kodekafe-spill/Assets/scripts/pauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pauseMenu : MonoBehaviour
+{
+
+    public GameObject menu;
+    public bool PauseMenu = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseMenu = !PauseMenu;
+            Time.timeScale = 0;
+            menu.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            if (PauseMenu != true)
+            {
+                Time.timeScale = 1;
+                menu.SetActive(false);
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+}
---
+++ kodekafe-spill/Assets/scripts/player/fov.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fov : MonoBehaviour
+{
+
+    public float FOV = 60f;
+    public Camera cam;
+
+    public Text fovText;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        fovText.text = FOV.ToString();
+    }
+
+    public void adjustFov (float newFov)
+    {
+        FOV = newFov;
+        cam.fieldOfView = newFov;
+    }
+}
