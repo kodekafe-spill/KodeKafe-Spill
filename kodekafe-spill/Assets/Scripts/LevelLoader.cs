3912ae98d9bc2cd7e20a82e4f6ec15b43a8d5e10
Nikolai Matre <32072697+ent3r@users.noreply.github.com>
2020-02-09 21:44:03 +0100

    Main menu

---
+++ kodekafe-spill/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+
+
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text text;
+
+    public void LoadLevel (int sceneIndex)
+    {
+        StartCoroutine(LoadAsyncronously(sceneIndex));
+    }
+
+
+    IEnumerator LoadAsyncronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            text.text = progress.ToString() + "%";
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+}
