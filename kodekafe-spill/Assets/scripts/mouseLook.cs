e3b6b0711fa825ba3d818fa90f589d48e3ff9762
Nikolai Dale Milde <ndalemilde@outlook.com>
2020-02-09 08:38:06 +0100

    Added clamping + mouse capture

--- kodekafe-spill/Assets/scripts/mouseLook.cs
+++ kodekafe-spill/Assets/scripts/mouseLook.cs
@@ -13,7 +13,7 @@ public class mouseLook : MonoBehaviour
 
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
@@ -22,6 +22,7 @@ public class mouseLook : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
 
         xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
