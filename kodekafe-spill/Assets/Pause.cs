be8600e837e547313e8d5acff93f5f5735fddcf7
Nikolai Matre <32072697+ent3r@users.noreply.github.com>
2020-02-10 21:35:52 +0100

    UI STUFFS
    FOV & Mouse sens funker fortsatt ikke

---
+++ kodekafe-spill/Assets/Pause.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsyncronously(sceneIndex));
+    }
+
+
+    IEnumerator LoadAsyncronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
--- kodekafe-spill/Assets/Scripts/Menu/Settings/SettingsMenu.cs
+++ kodekafe-spill/Assets/Scripts/Menu/Settings/SettingsMenu.cs
@@ -1,13 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class SettingsMenu : MonoBehaviour
 {
 
+    public TMP_Dropdown dropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        dropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].width == Screen.currentResolution.width)
+            {
+                currentResIndex = i;
+            }
+        }
+
+
+        dropdown.AddOptions(options);
+        dropdown.value = currentResIndex;
+        dropdown.RefreshShownValue();
+    }
+
+    public void SetRes(int resIndex)
+    {
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
     public void SetQuality (int qualInd)
     {
         QualitySettings.SetQualityLevel(qualInd);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetSensitivity(int sens)
+    {
+        PlayerPrefs.SetFloat("Sens", sens);
     }
 
 }
